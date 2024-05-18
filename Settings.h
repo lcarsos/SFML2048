b3e8dd1bfdba148f0590534fbb907891bfdfa825
me <malcolmflarose@gmail.com>
2024-05-17 20:12:55 -0400

    update

--- Settings.h
+++ Settings.h
@@ -3,6 +3,8 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+#include <SFML/Graphics.hpp>
+
 struct MySettings {
 
 	static MySettings& getInstance() {
@@ -32,28 +34,28 @@ struct MySettings {
 	static constexpr int windowMinHeight = totalHeight;
 
 	static struct colors2048 {
-		/*SDL_Color farBackColor = { 76, 76, 76, 100 };
-		SDL_Color backgroundColor = { 202, 190, 176, 255 };
-		SDL_Color gridColor = { 175, 161, 148, 255 };
+		sf::Color farBackColor = { 55, 55, 55, 255 };
+		sf::Color backgroundColor = { 202, 190, 176, 255 };
+		sf::Color gridColor = { 175, 161, 148, 255 };
 
 
-		SDL_Color cell2Color = { 233, 228, 218, 255 };
-		SDL_Color cell4Color = { 236,224,202,255 };
-		SDL_Color cell8Color = { 242,177,121,255 };
-		SDL_Color cell16Color = { 236,141,83,255 };
-		SDL_Color cell32Color = { 245,124,95,255 };
-		SDL_Color cell64Color = { 233,89,54,255 };
-		SDL_Color cell128Color = { 244,216,109 };
-		SDL_Color cell256Color = { 241,208,75,255 };
-		SDL_Color cell512Color = { 228,192,42,255 };
-		SDL_Color cell1024Color = { 237, 197,63,255 };
-		SDL_Color cell2048Color = { 236, 196, 5, 255 };
-		SDL_Color cell4096Color = { 60,116,50,255 };
-		SDL_Color cell8192Color = { 30, 29,25,255 };
+		sf::Color cell2Color = { 233, 228, 218, 255 };
+		sf::Color cell4Color = { 236,224,202,255 };
+		sf::Color cell8Color = { 242,177,121,255 };
+		sf::Color cell16Color = { 236,141,83,255 };
+		sf::Color cell32Color = { 245,124,95,255 };
+		sf::Color cell64Color = { 233,89,54,255 };
+		sf::Color cell128Color = { 244,216,109 };
+		sf::Color cell256Color = { 241,208,75,255 };
+		sf::Color cell512Color = { 228,192,42,255 };
+		sf::Color cell1024Color = { 237, 197,63,255 };
+		sf::Color cell2048Color = { 236, 196, 5, 255 };
+		sf::Color cell4096Color = { 60,116,50,255 };
+		sf::Color cell8192Color = { 30, 29,25,255 };
 
-		SDL_Color numberColor = { 245,245,250,255 };
+		sf::Color numberColor = { 245,245,250,255 };
 
-		SDL_Color scoreBGColor = {};*/
+		sf::Color scoreBGColor = {};
 	};
 
 private:
